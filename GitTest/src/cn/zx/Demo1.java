acaef269c044ef12898a79b1004d62f7dadf3c48
Administrator <Administrator@windows10.microdone.cn>
2019-08-06 11:28:06 +0800

    解决冲突

--- GitTest/src/cn/zx/Demo1.java
+++ GitTest/src/cn/zx/Demo1.java
@@ -5,7 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("zx");
 
-		System.out.println("333333");
+		System.out.println("666666");
 
 		System.out.println("111111");
 		System.out.println("222222");
